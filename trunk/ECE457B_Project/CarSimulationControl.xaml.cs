92527ee861418e6a70d1bec093559f4130150168
dan.r.robson@gmail.com <dan.r.robson@gmail.com@ae67c2be-d323-d542-8d3a-9df3473d7b59>
2012-03-24 19:40:05 +0000

    Adding project with system visualization control just statically drawing environment + car

---
+++ trunk/ECE457B_Project/CarSimulationControl.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ECE457B_Project
+{
+    /// <summary>
+    /// Interaction logic for CarSimulationControl.xaml
+    /// </summary>
+    public partial class CarSimulationControl : UserControl
+    {
+        public CarSimulationControl()
+        {
+            InitializeComponent();
+        }
+    }
+}

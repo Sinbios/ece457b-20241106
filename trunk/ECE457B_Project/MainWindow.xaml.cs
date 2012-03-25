00bd79c6e40b9e19c7d3e3c20a9688df9956f199
dan.r.robson@gmail.com <dan.r.robson@gmail.com@ae67c2be-d323-d542-8d3a-9df3473d7b59>
2012-03-25 00:10:43 +0000

    Updated application layout and background + road lines, and trees are now drawing programmatically

--- trunk/ECE457B_Project/MainWindow.xaml.cs
+++ trunk/ECE457B_Project/MainWindow.xaml.cs
@@ -22,6 +22,28 @@ namespace ECE457B_Project
         public MainWindow()
         {
             InitializeComponent();
+
+            this.Width = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Width - 10;
+            this.Height = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Height - 30;
+
+            this.SimulationPerformanceRow.Height = new GridLength(this.Height - this.SimulationInfoRow.Height.Value - this.TitleRow.Height.Value);
+
+            this.SystemVisualizationColumn.Width = new GridLength(this.Width - this.ParameterColumn.Width.Value);
+
+            CarSimulationControl instance = CarSimulationControl.CreateInstance(this.SystemVisualizationColumn.Width.Value, this.SimulationPerformanceRow.Height.Value);
+            instance.SetCurrentValue(Grid.ColumnProperty, 1);
+
+            this.SimulationParameterAndVisualiationGrid.Children.Add(instance);
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
